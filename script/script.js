3c90b64f64ab2b04c2ef80b371f6f90766f8b628
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2023-06-27 19:11:51 +0500

    add header and header-media

---
+++ script/script.js
@@ -0,0 +1,9 @@
+const menuBtn = document.querySelector('.menu-btn');
+const mobileNav = document.querySelector('.mobile-nav');
+
+
+
+menuBtn.addEventListener('click', () => {
+    mobileNav.classList.toggle('hidden')
+})
+
