5080994f96929b0a4b6e87780a502d38aab99773
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2023-06-28 13:32:40 +0500

    add header section (responsive)

--- script/script.js
+++ script/script.js
@@ -2,8 +2,8 @@ const menuBtn = document.querySelector('.menu-btn');
 const mobileNav = document.querySelector('.mobile-nav');
 
 
-
 menuBtn.addEventListener('click', () => {
     mobileNav.classList.toggle('hidden')
 })
+
 
