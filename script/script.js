c7a3b4cf3742ac924c42d1d8961f2f3c27522c44
bekzodxudaybergenow <bekzodxudaybergenow@gmail.com>
2023-07-01 18:34:42 +0500

    add header responsive style

--- script/script.js
+++ script/script.js
@@ -1,9 +1,19 @@
 const menuBtn = document.querySelector('.menu-btn');
 const mobileNav = document.querySelector('.mobile-nav');
+const header = document.querySelector('.header');
 
 
+// mobile-nav-btn
 menuBtn.addEventListener('click', () => {
     mobileNav.classList.toggle('hidden')
 })
 
-
+// mobile-nav-box-shadow
+window.addEventListener('scroll', (e) => {
+    if(window.pageYOffset > 0) {
+        header.classList.add('header-shadow');
+    }
+    else {
+        header.classList.remove('header-shadow');
+    }
+})
